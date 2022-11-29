6762b09ca405cc82a648363ba6ad6b20b7a38a9e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-29 11:45:41 +0100

    Minor change

--- src/lab4/js/main.js
+++ src/lab4/js/main.js
@@ -127,16 +127,16 @@ const createBarChart = (data) => {
   /* Create the bars and append them to the chart
     Ref: https://observablehq.com/@d3/enter-update-exit */
   
-  svg.selectAll("rect")
-  .data(data)
-  .join("rect")
-  .attr("x", d => xScale(d.country))
-  .attr("y", d => yScale(d.value))
-  .attr("width", xScale.bandwidth())
-  .attr("height", d => yScale(0) - yScale(d.value))
-  .attr("fill", d => colorScale(d.country))
-  .attr("stroke", "black")
-  .attr("stroke-width", 1);
+  // svg.selectAll("rect")
+  // .data(data)
+  // .join("rect")
+  // .attr("x", d => xScale(d.country))
+  // .attr("y", d => yScale(d.value))
+  // .attr("width", xScale.bandwidth())
+  // .attr("height", d => yScale(0) - yScale(d.value))
+  // .attr("fill", d => colorScale(d.country))
+  // .attr("stroke", "black")
+  // .attr("stroke-width", 1);
 
 
   /* Working with Color: https://observablehq.com/@d3/working-with-color 
