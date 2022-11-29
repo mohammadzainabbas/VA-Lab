4df0b4bf391cd7fe906dcd5b2a333fb2a78dad66
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-29 11:01:07 +0100

    Work in progress

--- src/lab4/js/main.js
+++ src/lab4/js/main.js
@@ -22,11 +22,10 @@ d3.csv(`/src/lab4/data/data.csv`, d3.autoType).then(data => {
 
   // Filter the data from the year 2020
   const data2020 = data.filter(d => d.year === 2020);
-  console.log(`Data from 2020: ${data2020}`);
   console.groupCollapsed("Data from 2020");
-  // console.group("Data from 2020");
   console.table(data2020)
   console.groupEnd()
+  
   // Sort the country by the percentage in the descending order
   const sortedData = data2020.sort((a, b) => d3.descending(a.value, b.value));
   console.log(`Sorted data: ${sortedData}`);
