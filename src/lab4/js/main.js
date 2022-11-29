1b02e1683eb42a15634bc456b3c8bd538d166e51
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-29 10:52:56 +0100

    Minor changes

--- src/lab4/js/main.js
+++ src/lab4/js/main.js
@@ -5,14 +5,15 @@
   Ref: https://www.d3indepth.com/requests/ */
 d3.csv(`/src/lab4/data/data.csv`, d3.autoType).then(data => {
   // Print out the data on the console
-  console.table(data);
+  console.log(data);
+  // console.table(data);
 
   /* Data Manipulation in D3 
     Ref: https://observablehq.com/@d3/d3-extent?collection=@d3/d3-array */
 
   // Get the minimum and maximum of the percent pay gap
-  const minPayGap = d3.min(data, d => +d.pay_gap);
-  const maxPayGap = d3.max(data, d => +d.pay_gap);
+  const minPayGap = d3.min(data, d => d.pay_gap);
+  const maxPayGap = d3.max(data, d => d.pay_gap);
 
   console.log(`Min pay gap: ${minPayGap}`);
   console.log(`Max pay gap: ${maxPayGap}`);
